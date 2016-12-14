9e6f3f8befc8d85c78f8906ca26ed885df79f352
Tom de Bruijn <tom@tomdebruijn.com>
2016-12-14 11:33:28 +0100

    Remove unused .mod-header_nav logic

--- source/javascripts/application.js
+++ source/javascripts/application.js
@@ -1,16 +1,4 @@
 $(document).ready(function(){
-  $('body').on('touchstart',function(){
-    $('.mod-header_nav').removeClass('show');
-  });
-  $('.mod-header_nav').on('touchstart', function(){
-    setTimeout(
-      function(){
-        $('.mod-header_nav').addClass('show');
-      }, 30);
-  });
-  $(window).on('resize',function(){
-    $('.mod-header_nav').removeClass('show');
-  });
   $(".mod-side_nav h2").on('click',function(){
     $(this).toggleClass("open");
   });
