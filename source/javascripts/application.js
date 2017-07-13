829a4a06dfd960bdf533787d00d6751fd21d10e1
Tom de Bruijn <tom@tomdebruijn.com>
2017-07-13 12:14:10 +0200

    Remember the scroll position of the navigation
    Closes #118

--- source/javascripts/application.js
+++ source/javascripts/application.js
@@ -2,6 +2,7 @@
 //= require navigation_store
 
 $(document).ready(function() {
+  var navigationElement = $("#navigation");
   var navigationElements = $(".mod-side_nav h2");
 
   // Open getting started on first visit
@@ -10,7 +11,7 @@ $(document).ready(function() {
   }
 
   // Make sure the section of the current page is open
-  var currentMenu = $("#navigation a.active").parents(".section").find("h2").data("menu");
+  var currentMenu = navigationElement.find("a.active").parents(".section").find("h2").data("menu");
   if(currentMenu) {
     NavigationStore.set(currentMenu, true);
   }
@@ -31,4 +32,18 @@ $(document).ready(function() {
       element.addClass("open");
     }
   });
+
+  window.NAVIGATION_SCROLL_TOP_KEY = "navigation_scroll_top";
+  function storeScrollPosition(position) {
+    localStorage.setItem(window.NAVIGATION_SCROLL_TOP_KEY, position);
+  }
+  function getScrollPosition() {
+    return localStorage.getItem(window.NAVIGATION_SCROLL_TOP_KEY);
+  }
+
+  // Save scroll position
+  $("#navigation").on("scroll", function(e) {
+    storeScrollPosition($(this).scrollTop());
+  });
+  navigationElement.scrollTop(getScrollPosition());
 });
