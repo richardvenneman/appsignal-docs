8221e01b912d258bd8a4ab352351f7b710f17bcf
Tom de Bruijn <tom@tomdebruijn.com>
2018-10-19 10:13:13 +0200

    Anonymize IP address for Google Analytics

--- source/javascripts/analytics.js
+++ source/javascripts/analytics.js
@@ -1,20 +1,4 @@
 function loadAnalyticsScripts() {
-  // Google Analytics
-  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-  })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
-  ga('create', 'UA-35502258-1', 'auto');
-  ga('send', 'pageview');
-
-  // LinkedIn
-  _linkedin_data_partner_id = "241314";
-  (function(){var s = document.getElementsByTagName("script")[0];
-  var b = document.createElement("script");
-  b.type = "text/javascript";b.async = true;
-  b.src = "https://snap.licdn.com/li.lms-analytics/insight.min.js";
-  s.parentNode.insertBefore(b, s);})();
-
   // Facebook
   !function(f,b,e,v,n,t,s)
   {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
